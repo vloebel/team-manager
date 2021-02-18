e2f0abae990fccdb367e3ecd3ac4fef2ddc67a68
Vicky Loebel <vickyloebel@gmail.com>
2021-02-18 10:18:52 -0700

    directory structure

---
+++ index.js
@@ -0,0 +1,110 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+class Employee {
+  constructor(name, id, email) {
+    this.namename = name;
+    this.id = id;
+    this.email = email;
+  }
+  getName() {
+    
+  }
+  getId() {
+    
+  }
+  getEmail() {
+    
+  }
+  getRole() {
+  return "Employee"
+  }
+}
+
+class Manager extends Employee {
+  constructor(officeNumber) {
+    this.officeNumber = officeNumber;
+  }
+  getRole() {
+    return "Manager";
+  }
+}
+
+class Engineer extends Employee{
+  constructor(github) { // user name
+    this.github = github;
+  }
+  getGithub() {
+    
+  }
+  getRole() {
+    return "Engineer";
+  }
+  }
+
+  class Intern extends Employee{
+    constructor(school) { // user name
+      this.school = school;
+    }
+    getSchool() {
+      
+    }
+    getRole() {
+      return "Intern";
+    }
+    }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+// Prompt the user
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    },
+    {
+      type: 'list',
+      message: 'What is your preferred method of communication?',
+      name: 'contact',
+      choices: ['email', 'phone', 'Slack', 'smoke signal']
+    },
+    {
+      type: 'checkbox',
+      message: 'What languages do you know?',
+      name: 'stack',
+      choices: ['HTML', 'CSS', 'JavaScript', 'SQL']
+    }
+  ]) // Write the user response to a file by chaining the below callback method to the prompt above.
+  .then(function(data) {
+    // Bonus: Generate the name of your user file from their input
+    const filename =
+      data.name
+        .toLowerCase()
+        .split(' ')
+        .join('') + '.json';
+// NOTE: STRINGIFY TAKES THE KEY/VALUE PAIRS AND TURNS THEM TAB DELIMITED STRINGS
+// JSON.stringify(value[, replacer[, space]]) 
+// value is the value to convert to a string - in this case, our data object
+// if replacer is null, all properties of the value object are included
+// space (or tab) is white space to insert. a number indicates a number of spaces <10
+//
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
+      if (err) {
+        return console.log(err);
+      }
+
+      console.log('Success!');
+    });
+  });
---
+++ src/page-template.js
@@ -0,0 +1,39 @@
+
+
+// export function to generate entire page
+module.exports = templateData => {
+  // destructure page data by section
+  const { projects, about, ...header } = templateData;
+
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  
+  <body>
+    <header>
+      <div class="container flex-row justify-space-between align-center py-3">
+        <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+        <nav class="flex-row">
+          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">GitHub</a>
+        </nav>
+      </div>
+    </header>
+    <main class="container my-5">
+      ${generateAbout(about)}
+      ${generateProjects(projects)}
+    </main>
+    <footer class="container text-center py-3">
+      <h3 class="text-dark">&copy;2020 by ${header.name}</h3>
+    </footer>
+  </body>
+  </html>
+  `;
+};
